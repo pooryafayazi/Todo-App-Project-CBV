addadfca4a471ca93cef46acc3ab0e35e57c67ef
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-13 16:38:16 +0330

    02.04 tasks py files

---
+++ core/tasks/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView
+
+urlpatterns = [
+    path('', TaskListView.as_view(), name='task_list'),
+    path('task/new/', TaskCreateView.as_view(), name='task_create'),
+    path('task/<int:pk>/edit/', TaskUpdateView.as_view(), name='task_edit'),
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
+]
