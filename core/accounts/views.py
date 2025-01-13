6d9f2838ca0b5c0d834a946b3b1ca09834047775
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-13 16:40:55 +0330

    02.05 accounts py files

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views import View
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
-# Create your views here.
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'accounts/login.html')
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('task_list')
+        return render(request, 'accounts/login.html', {'error': 'Invalid credentials'})
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
