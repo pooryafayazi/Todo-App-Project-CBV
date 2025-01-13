f411beda120f46dfc7c2910ac8bd366cbcf7124f
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-13 17:49:03 +0330

    02.06 templates

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,6 +1,9 @@
 from django.views import View
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 class LoginView(View):
     def get(self, request):
@@ -19,3 +22,19 @@ class LogoutView(View):
     def get(self, request):
         logout(request)
         return redirect('login')
+class RegisterView(FormView):
+    template_name = "accounts/register.html"
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy("task_list")
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterView, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect("task_list")
+        return super(RegisterView, self).get(*args, **kwargs)
