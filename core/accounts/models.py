62e13f43e701ee80433a67f20bf8d773bdefe4a1
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-14 09:36:36 +0330

    03.02 user model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,13 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+from django.utils.translation import gettext_lazy as _ # This is used to translate the string to the user's language
 # Create your models here.
 
-
+class UserManager(BaseUserManager):
+    '''
+    Custom user manager class
+    '''
+    def create_user(self, email, password, **extra_fields):
+        '''
+        Create and return a user with an email and password or extra data
+        '''
+        if not email:
+            raise ValueError(_('The Email field must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, email, password, **extra_fields):
+        '''
+        Create and return a superuser with an email and password or extra data
+        '''
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True'))
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
+    '''
+    Custom User model fore the Todo app that supports using email instead of username
+    '''
     email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)    
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
