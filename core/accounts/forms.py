e64df3e4c54e7b98962c3b7790e022ac68ecac00
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-15 17:15:46 +0330

    04.10.17 accounts.forms

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -1,41 +1,9 @@
-
-'''
+     
 from django import forms
 from .models import User
-
+            
 from django.contrib.auth import get_user_model
-User = get_user_model()
-
-class UserRegistrationForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
-        fields = ['email','password1','password2']
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-    
-'''
-        
-        
-from django import forms
-from .models import User
-
-from django import forms
-from .models import User  # Ensure this imports your custom User model
+from django.core.exceptions import ValidationError
 
 class RegistrationForm(forms.ModelForm):
     password1 = forms.CharField(widget=forms.PasswordInput)
@@ -54,21 +22,18 @@ class RegistrationForm(forms.ModelForm):
             raise forms.ValidationError("Passwords do not match")
 
     def save(self, commit=True):
-        user = super().save(commit=False)  # Create the user instance, but do not save yet
-        user.set_password(self.cleaned_data['password1'])  # Hash the password
+        user = super().save(commit=False)  
+        user.set_password(self.cleaned_data['password1'])  
         if commit:
-            user.save()  # Save the user instance
+            user.save()  
         return user
         
         
  
             
-            
-from django import forms
-from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
 
-User = get_user_model()  # This will use your custom User model
+
+User = get_user_model()  
 
 class LoginForm(forms.Form):
     email = forms.EmailField()
@@ -85,7 +50,6 @@ class LoginForm(forms.Form):
             except User.DoesNotExist:
                 raise ValidationError("Invalid email or password.")
 
-            # Verify the password
             if not user.check_password(password):
                 raise ValidationError("Invalid email or password.")
         
