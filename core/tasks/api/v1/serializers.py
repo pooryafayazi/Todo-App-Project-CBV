aa6538f3ff0b2edd78c743f4a078b14016e9aa36
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-16 13:19:16 +0330

    05.10 ModelSerializer

--- core/tasks/api/v1/serializers.py
+++ core/tasks/api/v1/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
+from ...models import Task
 
-
-class TaskSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    complete = serializers.BooleanField()
+# class TaskSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     complete = serializers.BooleanField()
+    
+    
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        # fields = '__all__'
+        fields = ['id','title', 'complete','active','created_date','due_date']
+        
