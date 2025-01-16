67d1f82c4c43d091acc9ae5b132fbe284c3dd6b5
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-16 17:53:27 +0330

    06.08 DefaultRouter

--- core/tasks/api/v1/urls.py
+++ core/tasks/api/v1/urls.py
@@ -1,17 +1,22 @@
 from django.urls import path
 # from .views import api_task_list_view
-from .views import TaskDetail,TaskViewSet
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('task', views.TaskViewSet, basename='task')
+
+
 app_name = 'api-v1'
 
 urlpatterns = [
-    # path('task/', api_task_list_view,name='api-task-list'),
-    # path('task/', TaskList, name='task-list'),
-    # path('task/', TaskList.as_view(), name='task-list'),
-    # path('task/<int:id>/', taskDetail, name='task-detail'),
-    # path('task/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
-    path('task/', TaskViewSet.as_view({'get':'list','post':'create'}), name='task-list'),
-    path('task/<int:pk>/', TaskViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}), name='task-list'),
-
-
-
+    # path('task/', views.api_task_list_view,name='api-task-list'),
+    # path('task/', views.TaskList, name='task-list'),
+    # path('task/', views.TaskList.as_view(), name='task-list'),
+    # path('task/<int:id>/', views.taskDetail, name='task-detail'),
+    # path('task/<int:pk>/', views.TaskDetail.as_view(), name='task-detail'),
+    # path('task/', views.TaskViewSet.as_view({'get':'list','post':'create'}), name='task-list'),
+    # path('task/<int:pk>/', views.TaskViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}), name='task-list'),
 ]
+
+urlpatterns += router.urls
