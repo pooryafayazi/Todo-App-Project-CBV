9c4ab0e77d0170df7dfdc10c369984141c283c80
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-16 11:49:23 +0330

    05.04 first api view

--- core/tasks/urls.py
+++ core/tasks/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import TasksListView,TaskCreateView,TaskUpdateView,TaskDeleteView,TaskCompleteView
-
+# from .views import TasksListView,TaskCreateView,TaskUpdateView,TaskDeleteView,TaskCompleteView
+from .views import api_task_list_view
 app_name = 'tasks'
 
 urlpatterns = [
-    path('', TasksListView.as_view(), name='list_view'),
-    path('create/', TaskCreateView.as_view(), name='task-create'),
-    path('edit/<int:pk>/', TaskUpdateView.as_view(), name='task-update'),
-    path('delete/<int:pk>/', TaskDeleteView.as_view(), name='task-delete'),
-    path('complete/<int:pk>/', TaskCompleteView.as_view(), name='task-complete'),
+    # path('', TasksListView.as_view(), name='list_view'),
+    # path('create/', TaskCreateView.as_view(), name='task-create'),
+    # path('edit/<int:pk>/', TaskUpdateView.as_view(), name='task-update'),
+    # path('delete/<int:pk>/', TaskDeleteView.as_view(), name='task-delete'),
+    # path('complete/<int:pk>/', TaskCompleteView.as_view(), name='task-complete'),
+    path('task/', api_task_list_view,name='api-task-list'),
 
 ]
--- core/tasks/views.py
+++ core/tasks/views.py
@@ -115,3 +115,20 @@ class TaskDeleteView(LoginRequiredMixin, DeleteView):
     success_url = '/'
 
     
+# API
+'''
+from django.http import HttpResponse
+def api_task_list_view(request):
+    return HttpResponse("ok")
+'''
+
+# API function base view
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+# @api_view('GET',)
+# @api_view(['GET','POST'])
+@api_view()
+def api_task_list_view(request):
+    return Response({'name':'poorya'})
