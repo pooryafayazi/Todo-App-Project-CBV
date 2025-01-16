0c3210f15a33e8149628ad556d85064d7b41d88a
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-16 19:51:37 +0330

    06.14 absolute url

--- core/tasks/api/v1/serializers.py
+++ core/tasks/api/v1/serializers.py
@@ -12,9 +12,15 @@ class TaskSerializer(serializers.ModelSerializer):
     # user = serializers.CharField(read_only=True)
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url =serializers.ReadOnlyField(source='get_absolute_api_url',read_only=True)
+    absolute_url = serializers.SerializerMethodField(method_name='get_abs_ur')
     class Meta:
         model = Task
         # fields = '__all__'
-        fields = ['user','id','title','snippet', 'complete','active','relative_url','created_date','due_date']
+        fields = ['user','id','title','snippet', 'complete','active','relative_url','absolute_url','created_date','due_date']
         # read_only_fields = ['user']
+                
+    # def get_absolute_url(self,obj):
+    def get_abs_ur(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
         
