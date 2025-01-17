6ecca12f714ba648f3e0ba644b31c3df8cbf8201
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-17 15:00:28 +0330

    06.20 filterbackend

--- core/tasks/api/v1/views.py
+++ core/tasks/api/v1/views.py
@@ -20,6 +20,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework import mixins
 from rest_framework import viewsets
 from .permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 '''
 # @api_view('GET',)
@@ -133,6 +134,8 @@ class TaskModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     serializer_class = TaskSerializer
     queryset = Task.objects.all() #Task.objects.filter(complete=False)
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['creator', 'title', 'complete', 'active']
     
 
 
