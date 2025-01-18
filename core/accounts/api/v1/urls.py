f49688c0df44a7e22d99374f6c0415d58d0c4ae5
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-18 10:22:39 +0330

    08.10 add jwt

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,)
+from rest_framework_simplejwt.views import TokenVerifyView
 # from rest_framework.authtoken.views import ObtainAuthToken
 
 
@@ -12,6 +14,12 @@ urlpatterns = [
     # login token
     path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login'),
     path('token/logout/', views.CustomDiscardAuthToken.as_view(), name='token-logout'),
+    
+    # login jwt
+    path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
+
     
     # change pasword
     # reset password
