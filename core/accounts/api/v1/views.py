50e79eb5eab734a489e27b7aedc154cc68d527a4
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-18 18:03:53 +0330

    08.19 sending email

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -8,6 +8,8 @@ from rest_framework.authtoken.views import ObtainAuthToken,APIView
 from rest_framework.authtoken.models import Token
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.shortcuts import get_object_or_404
+from django.core.mail import send_mail
+
 from ...models import Profile
 
 # from ...models import User
@@ -85,3 +87,15 @@ class ProfileApiView(generics.RetrieveUpdateAPIView):
         queryset = self.get_queryset()
         obj = get_object_or_404(queryset, user=self.request.user)
         return obj
+    
+class TestEmailSend(generics.GenericAPIView):
+    
+    def get(self, request, *args, **kwargs):
+        send_mail(
+            "Subject here",
+            "Here is the message.",
+            "from@example.com",
+            ["to@example.com"],
+            fail_silently=False,
+        )
+        return Response('Email sent')
