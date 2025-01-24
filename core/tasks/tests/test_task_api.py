c7da264320008094790dde276c7f279682d61a89
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-24 14:53:16 +0330

    comment def postgresql_db

--- core/tasks/tests/test_task_api.py
+++ core/tasks/tests/test_task_api.py
@@ -7,7 +7,7 @@ from accounts.models import User
 import psycopg
 import os
 
-
+"""
 @pytest.fixture(scope="session")
 def postgresql_db():
     connection = psycopg.connect(
@@ -19,7 +19,7 @@ def postgresql_db():
     )
     yield connection
     connection.close()
-
+"""
 
 @pytest.fixture
 def api_client():
