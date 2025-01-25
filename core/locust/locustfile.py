13f6d7e322722ce061517de220b8fe20218c3a27
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-25 21:10:23 +0330

    14.03 authorization for load test

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -1,4 +1,13 @@
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
-    pass
+
+    def on_start(self):
+        resopnse = self.client.post("/accounts/api/v1/jwt/create/", data={
+            "email": "admin@admin.com",
+            "password": "123"}).json()
+        self.client.headers = {'Authorization': f'Bearer {resopnse.get("access",None)}'}
+
+    @task
+    def task_list(self):
+        self.client.get("/api/v1/task/")
